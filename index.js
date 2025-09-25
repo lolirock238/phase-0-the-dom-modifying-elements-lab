47a8876479db269476b1121cf9a2fcd16898b8cc
Nicole kibe <nicolekibe822@gmail.com>
2025-09-25 14:12:32 +0300

    modify the changes made to index.js and track package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+// ✅ Create a new <h1> element and store it in newHeader
+const newHeader = document.createElement("h1");
+
+// ✅ Give the newHeader an id of "victory"
+newHeader.id = "victory";
+
+// ✅ Add the text "Joanne is the champion"
+newHeader.textContent = "Joanne is the champion";
+
+// ✅ Append the newHeader to the document body
+document.body.append(newHeader);
